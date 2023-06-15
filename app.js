592e144a089fa31bfd2915fbe5d006fd3a2a96ea
Daniel Gomes de Oliveira Pereira <d4nielgomesdeoliveira@gmail.com>
2023-06-15 17:27:06 -0300

    commit inicial

---
+++ app.js
@@ -0,0 +1,29 @@
+let express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+//DB Connection
+const conn = require("./db/conn");
+conn();
+
+
+//Routes
+const routes = require("./routes/router")
+
+app.use("/api", routes)
+
+
+
+
+
+
+
+
+
+app.listen(5000, function () {
+    console.log('server on')
+})
---
+++ routes/router.js
@@ -0,0 +1,20 @@
+const router = require("express").Router()
+
+// Services router
+const servicesRouter = require("./services");
+router.use("/", servicesRouter);
+
+
+
+//Party router
+const partyRouter = require("./parties");
+
+router.use("/", partyRouter);
+
+
+
+
+
+
+
+module.exports = router;
